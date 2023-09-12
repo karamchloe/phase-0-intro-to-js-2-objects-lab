54eb3385e90ab3dfd1db24f1bb8e0533690f6505
Chloe Park <kpark0119@gmail.com>
2023-09-12 16:23:40 -0400

    Updates Key and Value destructively and non-destructively

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: "11 Broadway"
+
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) { 
+    const newValue = {...employee, [key]:value}
+    //newValue[key] = value
+    return newValue
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, value){
+    
+    employee[name] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, name){
+    const deletedObject = {...employee}
+    delete deletedObject.name
+    return deletedObject
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name
+    return employee
+}
